989ebc836ed510e6bde2c196236170858fa1341b
Travis Farmer <tfarmer@axionbio.com>
2026-06-17 13:08:31 -0400

    Deleted test_auth.py and implemented similar function in data_fetcher.py to send credentials to get access token back from microsoft

--- data_fetcher.py
+++ data_fetcher.py
@@ -14,3 +14,21 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 GRAPH_BASE = "https://graph.microsoft.com"
+
+
+#Authentication function, sends credentials to get an access token and if fails, it prints the error message and exits.
+def get_access_token():
+    response = requests.post(
+        f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token",
+        data={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "scope": "https://graph.microsoft.com/.default",
+            "grant_type": "client_credentials"
+        }
+    )
+    if response.status_code != 200:
+        print(f"ERROR: Authentication failed ({response.status_code})")
+        print(response.json().get("error_description", ""))
+        exit(1)
+    return response.json()["access_token"]
